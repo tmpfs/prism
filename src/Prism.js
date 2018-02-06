0ebe94b8be8c44e37b48231b6b3633335b569dfd
muji <muji@tmpfs.org>
2018-02-06 15:58:02 +0800

    Move registerComponent to module.

--- src/Prism.js
+++ src/Prism.js
@@ -5,6 +5,8 @@ import {Rule, processor} from './Processor'
 
 import Namespace from './Namespace'
 import Plugin from './Plugin'
+import registerComponent from './registerComponent'
+
 import classNamePlugin from './className'
 import mapPropsToStylePlugin from './mapPropsToStyle'
 
@@ -20,8 +22,6 @@ import tintColor from './tintColor'
 import textTransform from './textTransform'
 
 import util from './util'
-
-const STYLE = 'style'
 
 const {
   isObject,
@@ -47,18 +47,6 @@ const Configuration = {
     'xx-large': 26
   }
 }
-
-const fnOrObj = {
-  fn: (o) => isFunction(o) || isObject(o),
-  type: 'function or object'
-}
-
-const mapPluginTypeTests = {
-  mapPropsToStyle: fnOrObj,
-  mapStyleToProps: fnOrObj
-}
-
-const mapPluginNames = Object.keys(mapPluginTypeTests)
 
 const registerPlugins = (plugins) => {
   return plugins.reduce((list, plugin) => {
@@ -107,145 +95,6 @@ const Prism = (Type, namespace = '', requirements = null) => {
   Prism.defined.push(definition)
 
   return NewType
-}
-
-const registerComponentPlugins = (registry, definition, options) => {
-  const {Type, Name} = definition
-  // Allow declaring mapPropsToStyle etc. as static on the Type
-  mapPluginNames.forEach((name) => {
-    if (options[name] !== undefined && Type[name] !== undefined) {
-      throw new Error(
-        `Prism you declared ${name} as static on ${Name} and also in styleOptions. ` +
-        `This is not allowed, choose one of the other declaration style.`)
-    }
-
-    // Get from static props
-    if (Type[name] !== undefined) {
-      options[name] = Type[name]
-      // Moving into the options, so clean up
-      delete Type[name]
-    }
-
-    // Call as function, supports a static declaration
-    // that can reference the styleSheet etc
-    if (util.isFunction(options[name])) {
-      options[name] = options[name](registry)
-    }
-
-    // Validate declared (or returned) types
-    const test = mapPluginTypeTests[name]
-    // Got a declaration, validate it
-    if (options[name] !== undefined && !test.fn(options[name])) {
-      throw new Error(
-        `Prism you declared ${name} as an invalid type, expected ${test.type} ` +
-        `but got ${typeof(options[name])}`
-      )
-    }
-  })
-
-  let {mapStyleToProps} = options
-  let childComponentNames = []
-  // User defined style property names
-  if (mapStyleToProps !== undefined) {
-    // Extract child component styles when a key is an object
-    let k, v
-    for (k in mapStyleToProps) {
-      v = mapStyleToProps[k]
-      if (isObject(v)) {
-        childComponentNames.push(k)
-      }
-    }
-  }
-
-  // Default style property support
-  options.mapStyleToProps = mapStyleToProps
-
-  options.allStyleObjectNames = [STYLE].concat(childComponentNames)
-  options.childComponentNames = childComponentNames
-}
-
-const registerComponentPropTypes = (definition, plugins, allStyleObjectNames) => {
-  const {Type} = definition
-  // Merge config propTypes into the Stylable propTypes.
-  //
-  // On collision the underlying component propTypes win.
-  const systemPropTypes = {}
-  plugins.forEach((plugin) => {
-    if (plugin.propType && !plugin.isGlobal) {
-      systemPropTypes[plugin.name] = plugin.propType
-    }
-  })
-  const propertyTypes = Object.assign(
-    {}, systemPropTypes, Type.propTypes)
-  Type.propTypes = propertyTypes
-
-  allStyleObjectNames.forEach((name) => {
-    // Automatic propTypes for style, labelStyle, imageStyle etc.
-    Type.propTypes[name] = propTypes.style
-  })
-}
-
-const splitPlugins = (plugins, options) => {
-  const globalPlugins = plugins
-    .filter((plugin) => {
-      if (plugin.isGlobal) {
-        // Global plugins that always execute
-        if (!plugin.requireOptions) {
-          return plugin
-        }
-
-        console.log('plugin name: ' + plugin.name)
-        console.log('plugin name: ' + options.hasOwnProperty(plugin.name))
-        console.log(Object.keys(options))
-
-        // Plugin requires a corresponding option
-        // in the target component options
-        if (plugins.requireOptions && options.hasOwnProperty(plugin.name)) {
-          return plugin
-        }
-      }
-    })
-  const propertyPlugins = plugins.filter(
-    (plugin) => !plugin.isGlobal)
-  return {
-    property: propertyPlugins,
-    globals: globalPlugins
-  }
-}
-
-const registerComponent = (registry, definition, config) => {
-  const {Type, Name, styleOptions} = definition
-  const {plugins} = config
-
-  let options = {}
-  if (styleOptions) {
-    // Get options for the component
-    options = styleOptions(registry)
-  }
-
-  if (options.registry && !(options.registry instanceof StyleRegistry)) {
-    throw new Error('Prism expects the registry option to be a StyleRegistry')
-  }
-
-  // Merge component-specific style registries
-  if (options.registry) {
-    registry.assign(options.registry)
-  }
-
-  // Split plugins and filter global plugins
-  // to those where the options have the corresponding
-  // configuration option
-  options.plugins = splitPlugins(plugins, options)
-
-  registerComponentPlugins(registry, definition, options)
-
-  // registerComponentPlugins must be called first
-  // so we have allStyleObjectNames
-  registerComponentPropTypes(definition, plugins, options.allStyleObjectNames)
-
-  definition.options = options
-  definition.config = config
-  definition.registry = registry
 }
 
 // All registered component definitions
@@ -347,6 +196,8 @@ Prism.configure = (registry, config = {}) => {
 
   // Iterate all defined components
   Prism.defined.forEach((definition) => {
+    definition.config = config
+    definition.registry = registry
 
     // Register the component definition
     registerComponent(registry, definition, config)
---
+++ src/registerComponent.js
@@ -0,0 +1,153 @@
+import propTypes from './propTypes'
+import util from './util'
+const {isObject} = util
+
+const STYLE = 'style'
+
+const fnOrObj = {
+  fn: (o) => isObject(o),
+  type: 'function or object'
+}
+
+const mapPluginTypeTests = {
+  mapPropsToStyle: fnOrObj,
+  mapStyleToProps: fnOrObj
+}
+
+const mapPluginNames = Object.keys(mapPluginTypeTests)
+
+const registerComponentPlugins = (registry, definition, options) => {
+  const {Type, Name} = definition
+  // Allow declaring mapPropsToStyle etc. as static on the Type
+  mapPluginNames.forEach((name) => {
+    if (options[name] !== undefined && Type[name] !== undefined) {
+      throw new Error(
+        `Prism you declared ${name} as static on ${Name} and also in styleOptions. ` +
+        `This is not allowed, choose one of the other declaration style.`)
+    }
+
+    // Get from static props
+    if (Type[name] !== undefined) {
+      options[name] = Type[name]
+      // Moving into the options, so clean up
+      delete Type[name]
+    }
+
+    // Call as function, supports a static declaration
+    // that can reference the styleSheet etc
+    if (util.isFunction(options[name])) {
+      options[name] = options[name](registry)
+    }
+
+    // Validate declared (or returned) types
+    const test = mapPluginTypeTests[name]
+    // Got a declaration, validate it
+    if (options[name] !== undefined && !test.fn(options[name])) {
+      throw new Error(
+        `Prism you declared ${name} as an invalid type, expected ${test.type} ` +
+        `but got ${typeof(options[name])}`
+      )
+    }
+  })
+
+  let {mapStyleToProps} = options
+  let childComponentNames = []
+  // User defined style property names
+  if (mapStyleToProps !== undefined) {
+    // Extract child component styles when a key is an object
+    let k, v
+    for (k in mapStyleToProps) {
+      v = mapStyleToProps[k]
+      if (isObject(v)) {
+        childComponentNames.push(k)
+      }
+    }
+  }
+
+  // Default style property support
+  options.mapStyleToProps = mapStyleToProps
+
+  options.allStyleObjectNames = [STYLE].concat(childComponentNames)
+  options.childComponentNames = childComponentNames
+}
+
+const registerComponentPropTypes = (definition, plugins, allStyleObjectNames) => {
+  const {Type} = definition
+  // Merge config propTypes into the Stylable propTypes.
+  //
+  // On collision the underlying component propTypes win.
+  const systemPropTypes = {}
+  plugins.forEach((plugin) => {
+    if (plugin.propType && !plugin.isGlobal) {
+      systemPropTypes[plugin.name] = plugin.propType
+    }
+  })
+  const propertyTypes = Object.assign(
+    {}, systemPropTypes, Type.propTypes)
+  Type.propTypes = propertyTypes
+
+  allStyleObjectNames.forEach((name) => {
+    // Automatic propTypes for style, labelStyle, imageStyle etc.
+    Type.propTypes[name] = propTypes.style
+  })
+}
+
+const splitPlugins = (plugins, options) => {
+  const globalPlugins = plugins
+    .filter((plugin) => {
+      if (plugin.isGlobal) {
+        // Global plugins that always execute
+        if (!plugin.requireOptions) {
+          return plugin
+        }
+
+        console.log('plugin name: ' + plugin.name)
+        console.log('plugin name: ' + options.hasOwnProperty(plugin.name))
+        console.log(Object.keys(options))
+
+        // Plugin requires a corresponding option
+        // in the target component options
+        if (plugins.requireOptions && options.hasOwnProperty(plugin.name)) {
+          return plugin
+        }
+      }
+    })
+  const propertyPlugins = plugins.filter(
+    (plugin) => !plugin.isGlobal)
+  return {
+    property: propertyPlugins,
+    globals: globalPlugins
+  }
+}
+
+const registerComponent = (registry, definition, config) => {
+  const {Type, Name, styleOptions} = definition
+  const {plugins} = config
+  let options = {}
+  if (styleOptions) {
+    // Get options for the component
+    options = styleOptions(registry)
+  }
+  if (options.registry && !(options.registry instanceof StyleRegistry)) {
+    throw new Error('Prism expects the registry option to be a StyleRegistry')
+  }
+  // Merge component-specific style registries
+  if (options.registry) {
+    registry.assign(options.registry)
+  }
+
+  // Split plugins and filter global plugins
+  // to those where the options have the corresponding
+  // configuration option
+  options.plugins = splitPlugins(plugins, options)
+
+  registerComponentPlugins(registry, definition, options)
+
+  // registerComponentPlugins must be called first
+  // so we have allStyleObjectNames
+  registerComponentPropTypes(definition, plugins, options.allStyleObjectNames)
+
+  definition.options = options
+}
+
+export default registerComponent
